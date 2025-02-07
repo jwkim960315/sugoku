f33e5ec36fb111efdfb1617788ecad793a4187f8
Jung Woo Kim <jwkim0315@gmail.com>
2025-02-06 22:21:26 -0800

    [M2][Feature] Add insertCells
    Description: This function creates and inserts cell to the provided table
    
    Tests: N/A

--- board/board.go
+++ board/board.go
@@ -2,6 +2,9 @@ package board
 
 import (
 	"github.com/gdamore/tcell/v2"
+	"github.com/jwkim960315/sugoku/cell"
+	"github.com/jwkim960315/sugoku/types"
+	"github.com/jwkim960315/sugoku/utils"
 	"github.com/rivo/tview"
 )
 
@@ -15,4 +18,14 @@ func customizeBoard(table *tview.Table) *tview.Table {
 								Foreground(tcell.ColorNone).
 				 				Background(tcell.ColorNone),
 				 )
+}
+
+func insertCells(table *tview.Table, boardData types.BoardData) {
+  for row := 0; row < utils.MaxNum; row++ {
+    for col := 0; col < utils.MaxNum; col++ {
+      textColor := cell.GetCellTextColor(&boardData[row][col], false)
+      tableCell := cell.GenerateCell(&boardData[row][col]).SetTextColor(textColor)
+      table.SetCell(row, col, tableCell)
+    }
+  }
 }
