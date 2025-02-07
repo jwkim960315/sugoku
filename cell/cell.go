3ed42de125f71686ecdf61ef414e361a5ceffce3
Jung Woo Kim <jwkim0315@gmail.com>
2025-02-06 19:01:30 -0800

    [M2][Feature] Add GenerateCell
    Description: This function simply creates a TableCell component. I'll be using this when creating a Sudoku board with Table
    
    Tests:
    ```
    go test -v ./cell
    
    === RUN   TestGenerateCellContent
    --- PASS: TestGenerateCellContent (0.00s)
    PASS
    ok      github.com/jwkim960315/sugoku/cell      0.191s
    ```

--- cell/cell.go
+++ cell/cell.go
@@ -2,8 +2,16 @@ package cell
 
 import (
 	"strconv"
+
+	"github.com/jwkim960315/sugoku/types"
+	"github.com/rivo/tview"
 )
 
 func GenerateCellContent(number int) string {
 	return " " + strconv.Itoa(number) + " "
+}
+
+func GenerateCell(cellData *types.CellData) *tview.TableCell {
+  str := GenerateCellContent(cellData.Number)
+  return tview.NewTableCell(str).SetAlign(tview.AlignCenter)
 }
