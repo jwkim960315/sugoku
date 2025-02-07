8dcd70044d73dfc897c2457ed1325df73fb6fa21
Jung Woo Kim <jwkim0315@gmail.com>
2025-02-06 22:11:08 -0800

    [M2][Feature] Add GetCellTextColor
    Description: This function returns a color of a cell text based on the cell data and whether it's selected or not. I might have to come back to this since I might add more conditions
    
    Tests:
    ```
    go test -v ./cell
    
    === RUN   TestGenerateCellContent
    --- PASS: TestGenerateCellContent (0.00s)
    PASS
    ok      github.com/jwkim960315/sugoku/cell      0.173s
    ```

--- cell/cell.go
+++ cell/cell.go
@@ -3,9 +3,22 @@ package cell
 import (
 	"strconv"
 
+	"github.com/gdamore/tcell/v2"
 	"github.com/jwkim960315/sugoku/types"
 	"github.com/rivo/tview"
 )
+
+func GetCellTextColor(cellData *types.CellData, selected bool) tcell.Color {
+	if selected {
+		return tcell.ColorRed
+	}
+
+	if cellData.Number == 0 {
+		return tcell.ColorReset
+	}
+
+	return tcell.ColorGreen
+}
 
 func GenerateCellContent(number int) string {
 	return " " + strconv.Itoa(number) + " "
